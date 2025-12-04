a3d696b2f09cce143086e480a2e8779157fb135a
katana1414 <katanakerf@gmail.com>
2025-12-04 16:43:13 +0300

    Create test_run.py

---
+++ zadanie-1/test_run.py
@@ -0,0 +1,10 @@
+from mentors import Mentor, Lecturer, Reviewer
+
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+
+print(isinstance(lecturer, Mentor))  # True
+print(isinstance(reviewer, Mentor))  # True
+
+print(lecturer.courses_attached)  # []
+print(reviewer.courses_attached)  # []
