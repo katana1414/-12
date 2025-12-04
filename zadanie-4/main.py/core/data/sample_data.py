7113cb20212ebec8fa363f0626addc5ec64f2d9e
katana1414 <katanakerf@gmail.com>
2025-12-04 17:07:56 +0300

    Create sample_data.py

---
+++ zadanie-4/main.py/core/data/sample_data.py
@@ -0,0 +1,12 @@
+from models.student import Student
+from models.lecturer import Lecturer
+
+students = [
+    Student("Alice", {"Math": [4, 5], "Physics": [3]}),
+    Student("Bob", {"Math": [5, 4], "Physics": [4, 4]})
+]
+
+lecturers = [
+    Lecturer("Dr. Smith", {"Math": [5, 4], "Physics": [4]}),
+    Lecturer("Dr. Johnson", {"Math": [3, 5], "Physics": [5, 4]})
+]
